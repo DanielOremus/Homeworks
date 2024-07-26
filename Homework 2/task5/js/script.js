f9cbb888a2092f801070ece345f3632d50164307
DanielOremus <oremusd89@gmail.com>
2024-07-26 11:21:44 +0300

    finished homework 2

---
+++ Homework 2/task5/js/script.js
@@ -0,0 +1,19 @@
+function getAvailableVehicle(category) {
+  const objArr = [
+    { category: "a", availableVehicle: "мотоцикл" },
+    { category: "b", availableVehicle: "легковий автомобіль" },
+    { category: "c", availableVehicle: "вантажний автомобіль" },
+  ];
+  const searchedObj = objArr.find((el) => el.category === category);
+  if (searchedObj) return searchedObj.availableVehicle;
+  throw new Error("Введіть коректне значення");
+}
+
+function showVehicle() {
+  const enteredCategory = document.querySelector("input").value.toLowerCase();
+  try {
+    alert(`Вам доступний ${getAvailableVehicle(enteredCategory)}`);
+  } catch (error) {
+    alert(error.message);
+  }
+}
